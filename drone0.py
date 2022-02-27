6daa8c14264ef87fedff14030e1b369a60c0133d
Steven Brewer <32825592+SBrewer15@users.noreply.github.com>
2022-02-27 14:18:26 -0500

    added position history

--- drone0.py
+++ drone0.py
@@ -7,13 +7,15 @@ import time
 
 util.set_seed(42)
 
-N_episodes=1
-episode_time=5
+N_episodes=2
+episode_time=60
+vehicle_name="Drone0"
 
-env=Environment(vehicle_name="Drone0", home=(15, -3, -7), maxz=120, maxspeed=8.33,episode_time=episode_time)
+env=Environment(vehicle_name=vehicle_name, home=(15, -3, -7), maxz=120, maxspeed=8.33,episode_time=episode_time)
 env.make_env()
 for episode in range(N_episodes):
     done=False
+    env.addWeather(weather=True, leaf=0.05, Roadwetness=0.1)
     env.StartTime(time.time())
     while done==False:
         action =np.random.randint(0, high=7, size=1, dtype=int)[0]
@@ -26,7 +28,9 @@ for episode in range(N_episodes):
 
 
         env.GetTime(time.time())
+
     print(f'Episode Number {episode+1} Complete')
+    env.df_gps.saveGPS2csv(f'data/gps_data_{vehicle_name}_episode{episode+1}.csv')
     if episode <N_episodes-1:
         #airsim.wait_key('Press any key to reset')
         print('Reset')
