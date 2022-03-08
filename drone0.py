49cfb4a08e7f0c70b1e9ebb1937188e6a21179c0
Steven Brewer <32825592+SBrewer15@users.noreply.github.com>
2022-03-07 20:44:56 -0500

    Updated reward function changed CNN
    removed pretrained mobilenet

--- drone0.py
+++ drone0.py
@@ -10,7 +10,7 @@ import os
 util.set_seed(42)
 
 N_episodes=1
-episode_time=10
+episode_time=900
 vehicle_name="Drone0"
 sz=(224,224)
 env=Environment(vehicle_name=vehicle_name, home=(15, -3, -7), maxz=120,
@@ -24,6 +24,11 @@ agent = DDQN(gamma=0.99, epsilon=1.0, lr=0.0001,
              chkpt_dir='models/', algo='DDQNAgent',
              env_name='Neighborhood')
 
+scores, eps_history, steps_array = [], [], []
+n_steps = 0
+load_checkpoint = False
+best_score = -np.inf
+
 for episode in range(N_episodes):
     score = 0
     done=False
@@ -32,16 +37,33 @@ for episode in range(N_episodes):
     np.save(f'data/FirstArray_{episode}', state)
     env.StartTime(time.time(), episode)
     while done==False:
-        #action =np.random.randint(0, high=3, size=1, dtype=int)[0]
+
         action = agent.choose_action(state)
+
         next_state, reward, done, info = env.step(action)
         score += reward
+        print(action, score)
+        if not load_checkpoint:
+            agent.store_transition(state, action,reward, next_state, int(done))
+            agent.learn()
+        state= next_state
+        n_steps += 1
+        scores.append(score)
+        steps_array.append(n_steps)
+
+        avg_score = np.mean(scores[-100:])
+
+        if avg_score > best_score:
+            if not load_checkpoint:
+                agent.save_models()
+            best_score = avg_score
+
+
 
         if done:
             np.save(f'data/LastArray_{episode}', next_state)
-
-
-
         env.GetTime(time.time())
-    print(f'Episode Number {episode+1} Complete')
-    #env.df_gps.saveGPS2csv(f'data/gps_data_{vehicle_name}_episode{episode+1}.csv')
+    print(f'Episode Number {episode+1} Complete. Final Score {score}, Number of steps taken: {n_steps}')
+    env.df_gps.saveGPS2csv(f'data/gps_data_{vehicle_name}_episode{episode+1}_DDQN.csv')
+    eps_history.append(agent.epsilon)
+    print(n_steps)
