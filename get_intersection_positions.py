3c1bcae08292dfa06847d966f3f52fcd9b499d75
Trevor Sherrard <sherrardtr@gmail.com>
2022-04-02 11:18:58 -0400

    draw missing lines

--- get_intersection_positions.py
+++ get_intersection_positions.py
@@ -6,6 +6,7 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+from shapely.geometry import LineString, Point
 
 filename = "data/intersection_positions.csv"
 
@@ -26,17 +27,30 @@ y_arr = np.array(y_vals)
 plt.plot(x_arr, y_arr)
 
 # plot points of interest
-points_of_interest = [(100, 125), (0, 125), 
+points_of_interest = [(125, 125), (0, 125), 
                       (-120, 125), (-125, 0), 
                       (-125, -125), (0, -125),
                       (125, -125), (125, 0), 
-                      (61, 1)]
+                      (75, 1), (80, -125),
+                      (0, 0)]
+
+across_horizontal = LineString([Point(-125, 0), Point(125, 0)])
+across_vert = LineString([Point(0, -125), Point(0, 125)])
+side_street = LineString([Point(81, -125), Point(75, -1)])
+missing_bottom = LineString([Point(125, 125), Point(125, 0)])
+
+plt.plot(*across_horizontal.xy)
+plt.plot(*across_vert.xy)
+plt.plot(*side_street.xy)
+plt.plot(*missing_bottom.xy)
 
 for pt in points_of_interest:
     label_str = "x: {}, y: {}".format(pt[0], pt[1])
-    plt.plot(pt[0], pt[1], marker='o', 
+    plt.plot(pt[0], pt[1], marker='o',
             markersize=3, color="red",
             label=label_str)
     plt.annotate(label_str, pt)
 
+
+plt.gca().invert_yaxis()
 plt.show()
